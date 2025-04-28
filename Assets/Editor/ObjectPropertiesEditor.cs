50fc308f669b8125e29c83a6509b1764a482f934
honey766 <honey76684@gmail.com>
2025-04-28 17:57:42 +0900

    [refactor] ObjectProperties.cs 레이어 자동 변경 기능추가
    ObjectProperties에 따라 오브젝트의 레이어가 Default 또는 Attachable로 자동 변경되도록 기능 추가
    
    공 또는 햄스터에서 그랩가능하면 Attachable 레이어,
    그랩 불가능하면 Default 레이어

--- Assets/Editor/ObjectPropertiesEditor.cs
+++ Assets/Editor/ObjectPropertiesEditor.cs
@@ -7,9 +7,13 @@ public class ObjectPropertiesEditor : Editor
     public override void OnInspectorGUI()
     {
         ObjectProperties prop = (ObjectProperties)target;
-        if (prop.canGrabInBallMode || prop.canGrabInHamsterMode)
+        if (prop.canGrabInBallMode || prop.canGrabInHamsterMode) {
             EditorGUILayout.HelpBox("Renderer/Materials에 2번째 머티리얼로 Outline 머티리얼을 할당해 주세요. \n게임 시작 시 DrawOutline.cs가 자동으로 생성됩니다.", MessageType.Info);
-
+            EditorGUILayout.HelpBox("게임 시작 시 Layer가 자동으로 Attachable로 변경됩니다.", MessageType.Info);
+        }
+        else {
+            EditorGUILayout.HelpBox("게임 시작 시 Layer가 자동으로 Default로 변경됩니다.", MessageType.Info);
+        }
         DrawDefaultInspector();
     }
 }
