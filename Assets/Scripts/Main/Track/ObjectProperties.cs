466748d227df182bd9dd5f55d222ce373d7e4ad4
honey766 <honey76684@gmail.com>
2025-04-28 17:40:23 +0900

    [feat] Add ObjectProperties.cs
    1. 플레이어가 오브젝트 위에서 점프 가능
    2. 공 모드에서 그랩 가능
    3. 햄스터 모드에서 그랩 가능
    
    세 가지 특성을 부여하는 스크립트.
    특성을 부여할 오브젝트에는 모두 해당 스크립트를 부착해야 함
    
    Editor : ObjectPropertiesEditor.cs 추가
    
    태그: Attachable, Pullable 제거
    레이어: Attachable만 남김

---
+++ Assets/Scripts/Main/Track/ObjectProperties.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+// 오브젝트의 특성
+// 해당 스크립트가 없다면 세 특성이 false인 것으로 간주
+// 특성에 따라 DrawOutline.cs는 자동으로 추가
+
+public class ObjectProperties : MonoBehaviour
+{
+    [Tooltip("플레이어가 오브젝트 위에서 점프 가능")]
+    public bool canPlayerJump = false;
+    [Tooltip("공 모드에서 그랩 가능한 오브젝트")]
+    public bool canGrabInBallMode = false;
+    [Tooltip("햄스터 모드에서 그랩 가능한 오브젝트")]
+    public bool canGrabInHamsterMode = false;
+
+    private void Start()
+    {
+        if (canGrabInBallMode || canGrabInHamsterMode) {
+            gameObject.AddComponent<DrawOutline>();
+        }
+    }
+}
