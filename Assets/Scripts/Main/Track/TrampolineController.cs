f4a5e5c159513d11c1763e6207056579998ecf01
honey766 <honey76684@gmail.com>
2025-05-03 19:19:47 +0900

    [refactor] modify ball player action
    공 와이어 액션이 시원시원하게 나가도록 개선
    공 상태의 공기저항이 상태에 따라 변함. (지상/공중/공중 와이어액션)
    
    트램펄린: 햄스터와 공기저항이 다르므로 반발계수를 나눔

--- Assets/Scripts/Main/Track/TrampolineController.cs
+++ Assets/Scripts/Main/Track/TrampolineController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class TrampolineController : MonoBehaviour
 {
     [Header("Values")]
-    [Tooltip("반발 계수")]
-    [SerializeField] private float bounceStrength = 1.6f;
+    [Tooltip("공 반발 계수")]
+    [SerializeField] private float bounceStrengthBall = 1.27f;
+    [Tooltip("햄스터 반발 계수")]
+    [SerializeField] private float bounceStrengthHamster = 1.6f;
 
     [Tooltip("Space로 더 높이 점프할 때 한정으로, y축 속도가 해당 값 이상이 된다면 더 높이 점프하지 않음")]
     [SerializeField] private float maxVelocityY = 50f;
@@ -13,6 +15,7 @@ public class TrampolineController : MonoBehaviour
     private bool isJump = false;
     private float jumpStartTime = 0;
     private float jumpBounceRate = 1.3f;
+    private float superJumpReactionTime = 0.1f; // Space 입력하고 이 시간 내로 트램펄린과 충돌해야 슈퍼점프 발동
 
     private void Update()
     {
@@ -20,7 +23,7 @@ public class TrampolineController : MonoBehaviour
             isJump = true;
             jumpStartTime = Time.time;
         }
-        else if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyUp(KeyCode.Space)) {
             isJump = false;
         }
     }
@@ -40,9 +43,10 @@ public class TrampolineController : MonoBehaviour
 
             // 충돌 직후 플레이어의 y축 속도 변경
             Vector3 newVel = preVelocity;
+            float bounceStrength = PlayerManager.instance.isBall ? bounceStrengthBall : bounceStrengthHamster;
             newVel.y = Mathf.Abs(newVel.y) * bounceStrength;
 
-            if (isJump && Time.time - jumpStartTime < 0.1f) {
+            if (isJump && Time.time - jumpStartTime < superJumpReactionTime) {
                 Debug.Log("트램펄린 슈퍼 점프 발동");
                 newVel.y = Mathf.Min(newVel.y * jumpBounceRate, maxVelocityY);
             }
