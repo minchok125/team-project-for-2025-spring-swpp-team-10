7304c2de6d54db7b7ae47fdaf910875eaaf1f0a3
Subeen Park <psb021414@gmail.com>
2025-05-10 19:20:42 +0900

    [feat] add inventory view to PlayerTest_KH1 scene

--- Assets/Scripts/Main/Item/ItemManager.cs
+++ Assets/Scripts/Main/Item/ItemManager.cs
@@ -4,37 +4,36 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    [SerializeField] private UI_InventoryItemPopup inventoryPopup;
+    // [SerializeField] private UI_InventoryItemPopup inventoryPopup;
     [SerializeField] private UI_InventoryZone inventoryZone;
     [SerializeField] private ItemInventory itemInventory;
-    [SerializeField] private CameraController cameraController;
 
     private void Awake()
     {
         itemInventory.items.ForEach(item =>
         {
-            item.count = 1;
+            item.count = 2;
         });
-        inventoryPopup.SetInfo(itemInventory);
+        // inventoryPopup.SetInfo(inventoryData);
         inventoryZone.SetInfo(itemInventory);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            bool isActive = !inventoryPopup.gameObject.activeSelf;
-            inventoryPopup.gameObject.SetActive(isActive);
-            cameraController.enabled = !isActive;
-            // 마우스 커서 활성화/비활성화
-            Cursor.visible = isActive;
-            Cursor.lockState = isActive ? CursorLockMode.None : CursorLockMode.Locked;
+        // TODO: 상점으로 변경 후 되살리기
+        // if (Input.GetKeyDown(KeyCode.H))
+        // {
+        //     bool isActive = !inventoryPopup.gameObject.activeSelf;
+        //     inventoryPopup.gameObject.SetActive(isActive);
+        //     // 마우스 커서 활성화/비활성화
+        //     Cursor.visible = isActive;
+        //     Cursor.lockState = isActive ? CursorLockMode.None : CursorLockMode.Locked;
 
-            if (inventoryPopup.gameObject.activeSelf)
-            {
-                inventoryPopup.RefreshUI(itemInventory);
-            }
-        }
+        //     if (inventoryPopup.gameObject.activeSelf)
+        //     {
+        //         inventoryPopup.RefreshUI(itemInventory);
+        //     }
+        // }
 
         if (Input.GetKeyDown(KeyCode.I))
         {
