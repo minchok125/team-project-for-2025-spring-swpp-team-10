0594074d0ceb12dde892b5982a7afbc0f6d5dbf9
leeSEOyoung <seosuper01@snu.ac.kr>
2025-05-15 01:24:56 +0900

    [U-08] 스피드런 기록 측정 구현 완료

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,68 +2,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
-    
-    [Header("References")]
-    [SerializeField] private AudioSource bgmSource;
-    [SerializeField] private AudioSource sfxSource;
-    
-    [Header("AudioClips")]
-    [SerializeField] private AudioClip[] bgmClips;
-    [SerializeField] private AudioClip[] sfxClips;
-    
-    [Header("Values")]
-    public float bgmVolume, sfxVolume;
-    
-    private PlayerData playerData;
-    
-    /* Scene 간의 이동 시 전달해야 하는 값들은 GameManager를 통해 보내면 됩니다
-     * 혼선 방지를 위해 팀별로 해당하는 곳 아래에 public으로 선언 부탁드립니다
-     * 프로그램 최초 시작 시 초기화해야 하는 값들은 Init()에서 설정해주시면 됩니다
-     *    (Scene 최초 시작이 아닌 프로그램 최초 시작인 점 주의!!)
-     * 추가할 method가 있다면 PlaySfx() 밑에 작성해주시면 됩니다
-     */
-    
-    // Player Action
-    
-    // Track
-    
-    // UI
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            Init();
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-    }
-
-    private void Init()
-    {
-        // 프로그램 최초 시작 시 초기화
-
-        // 1. 프레임 제한 설정
-        Application.targetFrameRate = 120;
-
-        // 2. 해상도 고정 (전체화면 모드: false -> 창모드, true -> 전체화면)
-        Screen.SetResolution(1280, 720, false);
-
-        // 3. VSync 끄기 (VSync가 켜져 있으면 targetFrameRate가 무시될 수 있음)
-        QualitySettings.vSyncCount = 0;
-    }
+  public static GameManager Instance { get; private set; }
   
-    public static void PlayBgm(int index)
+  [Header("References")]
+  [SerializeField] private AudioSource bgmSource;
+  [SerializeField] private AudioSource sfxSource;
+  
+  [Header("AudioClips")]
+  [SerializeField] private AudioClip[] bgmClips;
+  [SerializeField] private AudioClip[] sfxClips;
+  
+  [Header("Values")]
+  public float bgmVolume, sfxVolume;
+  
+  /* Scene 간의 이동 시 전달해야 하는 값들은 GameManager를 통해 보내면 됩니다
+   * 혼선 방지를 위해 팀별로 해당하는 곳 아래에 public으로 선언 부탁드립니다
+   * 프로그램 최초 시작 시 초기화해야 하는 값들은 Init()에서 설정해주시면 됩니다
+   *    (Scene 최초 시작이 아닌 프로그램 최초 시작인 점 주의!!)
+   * 추가할 method가 있다면 PlaySfx() 밑에 작성해주시면 됩니다
+   */
+  
+  // Player Action
+  
+  // Track
+  
+  // UI
+
+  private void Awake()
+  {
+    if (Instance == null)
     {
-        Instance.bgmSource.clip = Instance.bgmClips[index];
-        Instance.bgmSource.Play();
+      Instance = this;
+      Init();
+      DontDestroyOnLoad(gameObject);
     }
-    public static void StopBgm() { Instance.bgmSource.Stop(); }
-    public static void SetBgmVolume(float volume) { Instance.bgmVolume = volume; Instance.bgmSource.volume = volume; }
-    public static void PlaySfx(int index) { Instance.sfxSource.PlayOneShot(Instance.sfxClips[index]); }
-    public static void SetSfxVolume(float volume) { Instance.sfxVolume = volume; Instance.sfxSource.volume = volume; }
+    else Destroy(gameObject);
+  }
+
+  private void Init()
+  {
+    // 프로그램 최초 시작 시 초기화
     
-    /*****************************************************/
+    // 1. 프레임 제한 설정
+    Application.targetFrameRate = 120;
+
+    // 2. 해상도 고정 (전체화면 모드: false -> 창모드, true -> 전체화면)
+    Screen.SetResolution(1280, 720, false);
+    
+    // 3. VSync 끄기 (VSync가 켜져 있으면 targetFrameRate가 무시될 수 있음)
+    QualitySettings.vSyncCount = 0;
+  }
+  
+  public static void PlayBgm(int index)
+  {
+    Instance.bgmSource.clip = Instance.bgmClips[index];
+    Instance.bgmSource.Play();
+  }
+  public static void StopBgm() { Instance.bgmSource.Stop(); }
+  public static void SetBgmVolume(float volume)  { Instance.bgmSource.volume = volume; }
+  public static void PlaySfx(int index) { Instance.sfxSource.PlayOneShot(Instance.sfxClips[index]); }
+  public static void SetSfxVolume(float volume) { Instance.sfxVolume = volume; Instance.sfxSource.volume = volume; }
+  
+  /*****************************************************/
 }
--- Assets/Scripts/Main/MainSceneManager.cs
+++ Assets/Scripts/Main/MainSceneManager.cs
@@ -1,37 +1,111 @@
+using System;
 using UnityEngine;
 
 public class MainSceneManager : MonoBehaviour
 {
+    public static MainSceneManager Instance { get; private set; }
+    
     [Header("References")]
     [SerializeField] private UIManager uiManager;
     
+    private enum GameStates { Playing, Paused, BadEnding, GoodEnding };
+    private GameStates _gameState;
+    private float _timeRecord;
+
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            InitMainSceneManager();
+        }
+    }
+
+    private void InitMainSceneManager() // 게임 처음 시작 or 게임 재시작 시 초기화 되어야 하는 내용
+    {
+        uiManager?.InitUIManager();
+        _gameState = GameStates.Playing;
+        _timeRecord = 0f;
+        
         // 마우스 커서 숨기기
         Cursor.visible = false;
-        
         // 마우스 커서를 화면 중앙에 고정 (게임 창 내에서만 적용)
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Start()
+    private void Update()
     {
-        uiManager?.InitUIManager();
-    }
-
-    void Update()
-    {
+        // esc 키가 눌렸을 때 _gameState 값에 따라 Playing / Paused 상태 토글
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_gameState == GameStates.Playing) PauseGame();
+            else if (_gameState == GameStates.Paused) ResumeGame();
+        }
+        
+        // Playing일 때만 타이머 업데이트
+        if (_gameState == GameStates.Playing) UpdateTimer();
+        
         // ALT 키로 마우스 잠금 해제
-        if (Input.GetKey(KeyCode.LeftAlt) || Time.timeScale == 0)
+        if (Input.GetKey(KeyCode.LeftAlt))
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
-        // 마우스 잠금
-        else 
-        {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
+        
+        // Ending 잘 되는지 Debugging 용
+        if (Input.GetKeyDown(KeyCode.Backspace)) EndGame();
+    }
+
+    public void PauseGame()
+    {
+        // Playing 상태일 때 esc 키 눌리면 [Playing -> Paused]
+        _gameState = GameStates.Paused;
+        Time.timeScale = 0f;
+        uiManager.PauseGame();
+        
+        // 커서 보임
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        // Paused 상태일 때 esc 키 눌리면 [Paused -> Playing]
+        _gameState = GameStates.Playing;
+        Time.timeScale = 1f;
+        uiManager.ResumeGame();
+        
+        // 마우스 커서 잠금
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void EndGame()
+    {
+        // TODO: timeRecord에 따른 엔딩 분기 추가 필요
+        
+        // 게임 정지
+        _gameState = GameStates.BadEnding;
+        Time.timeScale = 0f;
+        
+        // 최종 분, 초, 밀리초 계산
+        _timeRecord += Time.deltaTime;
+        int minutes = (int)(_timeRecord / 60);
+        int seconds = (int)(_timeRecord % 60);
+        int milliseconds = (int)((_timeRecord * 100) % 100);
+        
+        uiManager.EndGame(minutes, seconds, milliseconds);
+        Debug.Log("End Game - " + _gameState);
+    }
+
+    private void UpdateTimer()
+    {
+        // 분, 초, 밀리초 계산
+        _timeRecord += Time.deltaTime;
+        int minutes = (int)(_timeRecord / 60);
+        int seconds = (int)(_timeRecord % 60);
+        int milliseconds = (int)((_timeRecord * 100) % 100);
+        // UI Manager를 통해 timer text 업데이트
+        uiManager.UpdateTimer(minutes, seconds, milliseconds);
     }
 }
