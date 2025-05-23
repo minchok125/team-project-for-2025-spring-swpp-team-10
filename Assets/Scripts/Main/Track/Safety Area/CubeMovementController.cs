2b42b5b53d3db924c2f63941e6e1684a85e3871a
honey766 <honey76684@gmail.com>
2025-05-24 05:41:12 +0900

    [feat] 큐브 구역 추가, 기타 기능 추가
    4x4x4 큐브 형태의 상자가 움직이는 구역을 추가했습니다. 기믹을 조금 더 넣어볼 예정입니다.
    -CubeStageManager
    -CubeMovementController
    
    와이어 버튼으로 클릭하는 빨간색 버튼을 만들었습니다. 와이어 버튼을 클릭하면 중간 세이브용 드론이 나타나도록 구현했습니다.
    -WireClickButton
    -ButtonClickAndAppearDrone
    
    점멸 플랫폼을 디더링 효과와 함께 새롭게 구현했습니다. 혹시 몰라 기존 함수를 남겨두고 새로 함수를 만들었습니다.
    -BlinkNewController

---
+++ Assets/Scripts/Main/Track/Safety Area/CubeMovementController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+using Hampossible.Utils;
+
+public class CubeMovementController : MonoBehaviour
+{
+    private BlinkNewController _blink;
+    private const float FADE_DURATION = 0.5f;
+
+    private void Start()
+    {
+        _blink = GetComponent<BlinkNewController>();
+        if (_blink == null)
+            HLogger.General.Warning("BlinkPlatformController을 추가해 주세요", this);
+    }
+
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Z))
+    //         MoveTo(Vector3.zero, 5f);
+    //     if (Input.GetKeyDown(KeyCode.X))
+    //         Rotate(Vector3.up, 1.5f);
+    //     if (Input.GetKeyDown(KeyCode.C))
+    //         Rotate(Vector3.down, 1.5f);
+    //     if (Input.GetKeyDown(KeyCode.V))
+    //         Rotate(Vector3.right, 1.5f);
+    //     if (Input.GetKeyDown(KeyCode.B))
+    //         Rotate(Vector3.forward, 1.5f);
+    //     if (Input.GetKeyDown(KeyCode.N))
+    //         Disappear(5f);
+    // }
+
+
+    public void MoveTo(Vector3 vec, float moveTime)
+    {
+        transform.DOMove(vec, moveTime)
+                 .SetEase(Ease.Linear)
+                 .SetUpdate(UpdateType.Fixed); // FixedUpdate 기반으로 실행
+    }
+
+    public void Rotate(Vector3 axis, float moveTime)
+    {
+        StartCoroutine(RotateCoroutine(axis, moveTime));
+    }
+
+    private IEnumerator RotateCoroutine(Vector3 axis, float moveTime)
+    {
+        float time = 0f;
+        Quaternion startRot = transform.rotation;
+        Quaternion endRot = Quaternion.AngleAxis(90f, axis) * startRot;
+
+        while (time < moveTime)
+        {
+            float t = time / moveTime;
+            transform.rotation = Quaternion.Slerp(startRot, endRot, t);
+            yield return null;
+            time += Time.deltaTime;
+        }
+
+        transform.rotation = endRot; // 마지막 오차 보정
+    }
+
+    public void Disappear(float disappearTime)
+    {
+        _blink.FadeOut(FADE_DURATION);
+        Invoke(nameof(Appear), disappearTime);
+    }
+
+    private void Appear()
+    {
+        _blink.FadeIn(FADE_DURATION);
+    }
+}
