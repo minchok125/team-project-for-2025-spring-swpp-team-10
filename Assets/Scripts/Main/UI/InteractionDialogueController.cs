a9fc9d1ce978ee8fd21d73c7709ddf282046d5e9
honey766 <honey76684@gmail.com>
2025-06-01 02:00:41 +0900

    [refactor] InteractionDialogueController 주석 추가
    InteractionDialogueController.cs 주석을 추가하고 인스펙터 부분을 다듬었습니다.

--- Assets/Scripts/Main/UI/InteractionDialogueController.cs
+++ Assets/Scripts/Main/UI/InteractionDialogueController.cs
@@ -10,21 +10,29 @@ using UnityEditor;
 // public void DoDialogue()로 외부해서 대사 호출이 가능합니다.
 public class InteractionDialogueController : MonoBehaviour
 {
+    [Tooltip("대사를 출력한 직후 오브젝트를 삭제합니다.")]
     [SerializeField] private bool destroyThis = false;
     [Tooltip("같은 내용을 출력하는 최소 간격")]
     [SerializeField] private float minimumNotificationCooldown = 0.1f;
     [Tooltip("트리거면 true, 콜라이더면 false")]
     [SerializeField] private bool isTrigger = true;
+
+    [Header("대사 설정")]
+    [Tooltip("Oneline 대사인지, 아니면 파일 단위의 대사인지 선택합니다.")]
     [SerializeField] private bool isOnelineDialogue = false;
+    [Tooltip("Oneline 파일 안에 있는 대사인지, 아니면 직접 내용을 작성할지 선택합니다.")]
     [SerializeField] private bool isOnelineFileDialogue = false;
 
     [SerializeField] private string character;
     [SerializeField] private string text;
+    [Tooltip("대사가 화면에 표시되는 시간. 이 시간이 지나면 대사가 자동으로 사라집니다.")]
     [SerializeField] private float lifetime;
 
     [SerializeField] private string fileName;
+    [Tooltip("Oneline 파일의 인덱스 번호")]
     [SerializeField] private int index;
 
+    [Header("카메라 연출 설정")]
     [Tooltip("트리거 입장 시 대사와 함께 VirtualCamera 연출을 사용할 것인지 여부")]
     [SerializeField] private bool useVirtualCamera;
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
@@ -69,6 +77,7 @@ public class InteractionDialogueController : MonoBehaviour
         DoDelete();
     }
 
+    // 대사 출력 함수
     public void DoDialogue()
     {
         if (!_canInteract)
@@ -76,11 +85,13 @@ public class InteractionDialogueController : MonoBehaviour
 
         if (isOnelineDialogue)
         {
+            // Oneline 파일
             if (isOnelineFileDialogue)
             {
                 UIManager.Instance.DoDialogue(index);
                 HLogger.General.Info("Oneline 파일의 index번째 대사를 출력합니다.", this);
             }
+            // Oneline 커스텀
             else
             {
                 UIManager.Instance.DoDialogue(character, text, lifetime);
@@ -94,7 +105,7 @@ public class InteractionDialogueController : MonoBehaviour
         }
 
         _canInteract = false;
-        Invoke(nameof(CanInteractTrue), minimumNotificationCooldown);
+        Invoke(nameof(CanInteractTrue), minimumNotificationCooldown); // n초 뒤 다시 출력 가능
 
         DoCamera();
     }
@@ -104,13 +115,18 @@ public class InteractionDialogueController : MonoBehaviour
         _canInteract = true;
     }
 
+
+    // 카메라 연출 함수
     private void DoCamera()
     {
         if (!useVirtualCamera)
             return;
 
+        // 해당 카메라의 우선순위를 높여서 이 카메라의 화면이 보이도록 함
         virtualCamera.Priority = 11;
+        // 마우스 입력 잠금
         PlayerManager.Instance.SetMouseInputLockDuringSeconds(cameraShotTime + 2f);
+        // 카메라의 우선순위를 다시 낮춤
         Invoke(nameof(ChangeCameraHamsterPriorityToNine), cameraShotTime);
     }
 
@@ -120,6 +136,7 @@ public class InteractionDialogueController : MonoBehaviour
     }
 
 
+    // 대사 출력 후 게임오브젝트 삭제 함수
     private void DoDelete()
     {
         if (!destroyThis)
