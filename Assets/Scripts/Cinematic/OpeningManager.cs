572c856ea95e7bf0fda94f8b3d53e98d40262c63
leeSEOyoung <seosuper01@snu.ac.kr>
2025-06-02 15:44:27 +0900

    [feat] 오프닝 컷신 카메라 위치

--- Assets/Scripts/Cinematic/OpeningManager.cs
+++ Assets/Scripts/Cinematic/OpeningManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class OpeningManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("References")]
+    [SerializeField] private Camera cam1;
+    [SerializeField] private Camera cam2;
+    [SerializeField] private Camera cam3;
+    [SerializeField] private Camera cam4;
+    [SerializeField] private Camera cam5;
+    [SerializeField] private Camera cam6;
+    
+    
+    [Header("CutScene")]
+    [SerializeField] private Vector2 safeCutPos, safeCutSize;
+    [SerializeField] private Vector2 hamsterCutPos, hamsterCutSize;
+    [SerializeField] private Vector2 roomCutPos, roomCutSize;
+    [SerializeField] private Vector2 kitchenCutPos, kitchenCutSize;
+    [SerializeField] private Vector2 livingRoomCutPos, livingRoomCutSize;
+    [SerializeField] private Vector2 bathroomCutPos, bathroomCutSize;
+    
+    
+    private void Awake()
     {
-        
+        cam1.enabled = true;
+        cam2.enabled = true;
+        cam3.enabled = true;
+        cam4.enabled = true;
+        cam5.enabled = true;
+        cam6.enabled = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DebugCamRect()
     {
-        
+        cam1.rect = new Rect(safeCutPos, safeCutSize);
+        cam2.rect = new Rect(hamsterCutPos, hamsterCutSize);
+        cam3.rect = new Rect(roomCutPos, roomCutSize);
+        cam4.rect = new Rect(kitchenCutPos, kitchenCutSize);
+        cam5.rect = new Rect(livingRoomCutPos, livingRoomCutSize);
+        cam6.rect = new Rect(bathroomCutPos, bathroomCutSize);
     }
+    
 }
