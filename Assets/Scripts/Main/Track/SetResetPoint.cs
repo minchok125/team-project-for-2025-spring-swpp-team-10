5c5ec08ad1f8a0a9e39aaf1202f7f0ebf87222c9
honey766 <honey76684@gmail.com>
2025-06-06 23:18:55 +0900

    [feat] 뉴 햄스터 액션 적용, 풍선 적용
    - 뉴 햄스터 메쉬에 맞게 액션을 적용했습니다.
    - 글라이딩 효과인 풍선 애니메이션을 적용했습니다.

--- Assets/Scripts/Main/Track/SetResetPoint.cs
+++ Assets/Scripts/Main/Track/SetResetPoint.cs
@@ -13,5 +13,13 @@ public class SetResetPoint : MonoBehaviour
             return;
 
         PlayerManager.Instance.transform.position = resetPoint.position;
+        Rigidbody rb = PlayerManager.Instance.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero; // 회전 속도도 초기화
+        PlayerManager.Instance.GetComponent<PlayerWireController>().EndShoot(); // 와이어 사용 중이었다면 취소
+        PlayerManager.Instance.isJumping = false;
+        PlayerManager.Instance.isGliding = false;
+        PlayerManager.Instance.playerMovement.EndGliding();
+        PlayerManager.Instance.isBoosting = false; // 부스트 상태 해제
     }
 }
