70c25e00f54bbef553bbdc7fbe9bf52a3c49db91
dohyuneul <ydh0455@snu.ac.kr>
2025-06-22 19:13:00 +0900

    [feat] coin grade 통합
    coin grade와 controller를 연결하였습니다.

--- Assets/Scripts/Main/Item/Models/Coin.cs
+++ Assets/Scripts/Main/Item/Models/Coin.cs
@@ -1,15 +1,11 @@
 using UnityEngine;
 
-public enum CoinGrade
-{
-    Silver,
-    Gold
-}
-
+// 이 속성을 추가하여 Unity 인스펙터에서 Coin 클래스의 필드를 직접 보고 수정할 수 있게 합니다.
+[System.Serializable] 
 public class Coin
 {
     [Tooltip("코인 등급")]
-    public CoinGrade grade;
+    public CoinGrade grade; // 인스펙터에서 등급을 선택할 수 있습니다.
 
     public int Value
     {
@@ -24,6 +20,7 @@ public class Coin
         }
     }
 
+    // 이 팩토리 메서드는 코드 기반으로 코인을 생성할 때 여전히 유용합니다.
     public static Coin Create(
         CoinGrade grade
     )
@@ -33,5 +30,10 @@ public class Coin
             grade = grade
         };
     }
+}
 
+public enum CoinGrade
+{
+    Silver,
+    Gold
 }
