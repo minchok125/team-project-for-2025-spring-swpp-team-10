8ced5ddc4da49d962495be7aa074f0de037b3dc4
leeSEOyoung <seosuper01@snu.ac.kr>
2025-06-22 20:06:47 +0900

    [feat] Title 전환 시 fade 효과 추가

--- Assets/Scripts/Title/TitleSceneManager.cs
+++ Assets/Scripts/Title/TitleSceneManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,6 +11,21 @@ public class TitleSceneManager : MonoBehaviour
 {
     [SerializeField] TMP_InputField nameInputField;
     [SerializeField] GameObject notAcceptTxt;
+    [SerializeField] private Image fadePanel;
+
+    private void Awake()
+    {
+        fadePanel.color = Color.clear;
+        fadePanel.gameObject.SetActive(false);
+    }
+
+    private IEnumerator LoadSceneCoroutine()
+    {
+        fadePanel.gameObject.SetActive(true);
+        fadePanel.DOColor(Color.black, 3f);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(2);
+    }
 
     public void GoToOpening(int idx)
     {
@@ -21,6 +38,6 @@ public class TitleSceneManager : MonoBehaviour
 
         PersistentDataManager.Instance.mainSceneIndex = idx;
         PersistentDataManager.Instance.playerName = name;
-        SceneManager.LoadScene(2);
+        StartCoroutine(LoadSceneCoroutine());
     }
 }
