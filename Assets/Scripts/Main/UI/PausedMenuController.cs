dcb98c0e3fd941bd07eba8f5f6b479cbad89bf45
dohyuneul <ydh0455@snu.ac.kr>
2025-06-22 17:06:51 +0900

    [feat] 설정 UI 개선 및 카메라 설정 추가
    settings에 ui 테마가 적용되지 않았던 문제를 수정했습니다.
    마우스 감도와 줌 감도가 설정에 추가되었습니다.

--- Assets/Scripts/Main/UI/PausedMenuController.cs
+++ Assets/Scripts/Main/UI/PausedMenuController.cs
@@ -8,12 +8,24 @@ public class PausedMenuController : MonoBehaviour
 	// 게임 플레이 중 esc 키를 눌렀을 때 표시되는 Paused Menu의 Controller
 
 	[Header("References")]
-	[SerializeField] private UIManager uIManager;
+	[SerializeField] private UIManager UIManager;
 	[SerializeField] private GameObject settingsPanel;
-	[SerializeField] private Slider bgmVolumeSlider, sfxVolumeSlider;
+
+	[Header("Audio Settings")]
+	[SerializeField] private Slider bgmVolumeSlider;
+	[SerializeField] private Slider sfxVolumeSlider;
+
+	[Header("Camera Settings")]
+	[SerializeField] private Slider horizontalSensitivitySlider;
+	[SerializeField] private Slider verticalSensitivitySlider;
+	[SerializeField] private Slider zoomSensitivitySlider;
+
+	private CinemachineCameraManager cameraManager;
 
 	private void Awake()
 	{
+		cameraManager = CinemachineCameraManager.Instance;
+
 		settingsPanel.SetActive(false);
 
 		bgmVolumeSlider.value = AudioManager.Instance.BgmVolume;
@@ -22,9 +34,36 @@ public class PausedMenuController : MonoBehaviour
 		bgmVolumeSlider.onValueChanged.AddListener(AudioManager.Instance.SetBgmVolume);
 		sfxVolumeSlider.onValueChanged.AddListener(AudioManager.Instance.SetSfxVolume);
 
-		if (uIManager == null)
+		if (cameraManager != null)
 		{
-			Debug.LogWarning($"{gameObject.name} : uIManager이 null입니다.");
+			horizontalSensitivitySlider.value = cameraManager.MouseHorizontalSensitivity;
+			verticalSensitivitySlider.value = cameraManager.MouseVerticalSensitivity;
+			zoomSensitivitySlider.value = cameraManager.ZoomSensitivity;
+
+			horizontalSensitivitySlider.onValueChanged.AddListener(cameraManager.SetHorizontalSensitivity);
+			verticalSensitivitySlider.onValueChanged.AddListener(cameraManager.SetVerticalSensitivity);
+			zoomSensitivitySlider.onValueChanged.AddListener(cameraManager.SetZoomSensitivity);
+		}
+		else
+		{
+			Debug.LogWarning($"{gameObject.name} : CinemachineCameraManager가 null입니다.");
+		}
+
+		if (UIManager == null)
+		{
+			Debug.LogWarning($"{gameObject.name} : UIManager이 null입니다.");
+		}
+	}
+
+	public void ResetCameraSettings()
+	{
+		if (cameraManager != null)
+		{
+			cameraManager.ResetCameraSettings();
+
+			horizontalSensitivitySlider.value = cameraManager.MouseHorizontalSensitivity;
+			verticalSensitivitySlider.value = cameraManager.MouseVerticalSensitivity;
+			zoomSensitivitySlider.value = cameraManager.ZoomSensitivity;
 		}
 	}
 
@@ -41,34 +80,23 @@ public class PausedMenuController : MonoBehaviour
 	public void OpenStore()
 	{
 		HLogger.General.Debug("Paused Menu: 상점 열기");
-		uIManager.OpenStore();
+		UIManager.OpenStore();
 	}
 
 	public void CloseStore()
 	{
-		uIManager.CloseStore();
-	}
-
-	public void OpenGuide() 
-	{
-		HLogger.General.Debug("Paused Menu: 가이드 열기");
-		uIManager.OpenGuide();
-	}
-
-	public void CloseGuide()
-	{
-		uIManager.CloseGuide();
+		UIManager.CloseStore();
 	}
 
 	public void GoBackToTitle()
 	{
 		HLogger.General.Debug("Paused Menu: 타이틀로 돌아가기");
-		uIManager.QuitMainScene(false);
+		UIManager.QuitMainScene(false);
 	}
 
 	public void TerminateGame()
 	{
 		HLogger.General.Debug("Paused Menu: 게임 종료");
-		uIManager.QuitMainScene(true);
+		UIManager.QuitMainScene(true);
 	}
 }
